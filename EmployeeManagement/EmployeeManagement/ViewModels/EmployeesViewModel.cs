d9e8e8a94071859386b13053c8344dfa82dc1b68
aveA <aveA@AVE>
2025-07-20 13:39:18 +0300

    wpf start

---
+++ EmployeeManagement/EmployeeManagement/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagement.Models;
+using System.Collections.ObjectModel;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        private EmployeeRepository _employeeRepository { get; }
+
+        public EmployeesViewModel()
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => new(_employeeRepository.GetAll());
+        }
+    }
+}
